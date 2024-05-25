c82588d65c0dcbd23d3a769397fcacc841f2a5e7
anamsilva1981 <ana.msilva@hotmail.com>
2024-05-24 21:00:54 -0300

    feat: instalação e configuração do primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FilterMatchMode, PrimeNGConfig } from 'primeng/api';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'desapega';
+export class AppComponent implements OnInit{
+ 
+  private primengConfig = inject(PrimeNGConfig);
+
+  ngOnInit() {
+    this.autoZIndex();
+    this.filterMode();
+    this.locale();
+  }
+
+  public autoZIndex(){
+    this.primengConfig.zIndex = {
+      modal: 1100,    // dialog, sidebar
+      overlay: 1000,  // dropdown, overlaypanel
+      menu: 1000,     // overlay menus
+      tooltip: 1100   // tooltip
+  };
+  }
+
+  public filterMode(){
+    this.primengConfig.filterMatchModeOptions = {
+      text: [FilterMatchMode.STARTS_WITH, FilterMatchMode.CONTAINS, FilterMatchMode.NOT_CONTAINS, FilterMatchMode.ENDS_WITH, FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS],
+      numeric: [FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS, FilterMatchMode.LESS_THAN, FilterMatchMode.LESS_THAN_OR_EQUAL_TO, FilterMatchMode.GREATER_THAN, FilterMatchMode.GREATER_THAN_OR_EQUAL_TO],
+      date: [FilterMatchMode.DATE_IS, FilterMatchMode.DATE_IS_NOT, FilterMatchMode.DATE_BEFORE, FilterMatchMode.DATE_AFTER]
+  };
+  }
+
+  public locale(){
+    this.primengConfig.setTranslation({
+      accept: 'Accept',
+      reject: 'Cancel',
+      //translations
+  });
+  }
 }
